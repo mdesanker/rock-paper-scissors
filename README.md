aa4a8cc9a90e49e478d75ff655604574220cad9a
mdesanker <mdesanker@gmail.com>
2021-09-20 11:39:15 -0400

    Cleaned code a bit, added some button effects, updated README.md

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
 Rock Paper Scissors game made in JavaScript. First JS project as part of The Odin Project.
+
+Created a basic UI with 3 buttons to allow user to select which move they wanted to use. Game results and info to player is output into a div. 
+
+I learned how to use JavaScript to write basic functions, control html elements, apply css styling to elements, write DOM methods to make text appear on the screen in response to an event (e.g. button click), and disable buttons once the game is over to prevent user from playing.
+
+I think the way I set up the DOM methods was very tedious and probably not the most elegant solution. Maybe I will learn more efficient ways to handle this as I get more experience with JavaScript.
--- script.js
+++ script.js
@@ -3,45 +3,27 @@
 // Function that returns the computer's choice: rock, paper, or scissors
 function computerPlay() {
     let random_number = Math.floor(Math.random(3) * 3);  // Set variable number to random integer between 0 and 2
-    let choices = ["rock", "paper", "scissors"];
+    const choices = ["rock", "paper", "scissors"];
     let computerChoice = choices[random_number];   // Use random number to choose item from list of choices
     return computerChoice
 }
 
+// Function to take player choice and computer choice and determine who wins the round
+// Returns true if player wins and returns false if computer wins
 function playRound(playerSelection, computerSelection) {
     if (playerSelection === computerSelection) {    // Check for a tie
-        console.log(`You chose: ${playerSelection} and your opponent chose: ${computerSelection}. You tied.`);
-        return false
+        return false;
     } else if ((playerSelection === "rock" && computerSelection === "scissors")     // Check for wins
     || (playerSelection === "scissors" && computerSelection === "paper") 
     || (playerSelection === "paper" && computerSelection === "rock")) {
-        console.log(`You chose: ${playerSelection} and your opponent chose: ${computerSelection}. You win!`);
-        return true
+        return true;
     } else {    // If not a tie game or a win, it's a loss
-        console.log(`You chose: ${playerSelection} and your opponent chose: ${computerSelection}. You Lose.`)
-        return false
+        return false;
     }
 }
 
-function game() {
-    let num_games = 0;
-    let player_wins = 0;
-    while (num_games < 5) {     // Play through 5 games
-        playerChoice = prompt("Enter 'rock', 'paper', or 'scissors': ")
-        if (playRound(playerChoice, computerPlay())) { // playRound function returns true or false
-            player_wins++;    // Player score only increases on a win
-            num_games++;
-        } else {
-            num_games++;
-        }
-    }
-    console.log(`You won ${player_wins} out of ${num_games} games`)     // Display how many games player won
-}
-
-// game()
-
-let playerScore = 0
-let computerScore = 0
+let playerScore = 0;
+let computerScore = 0;
 
 const outputWindow = document.querySelector('.output');
 
@@ -59,23 +41,24 @@ buttons.forEach((button) => {
         displayPlayerChoice.textContent = `You chose: ${playerChoice}`;
         displayComputerChoice.textContent = `Computer chose: ${computerChoice}`;
 
-        if (playerChoice === computerChoice) {
+        if (playerChoice === computerChoice) {  // Check for tie
             displayRoundWinner.textContent = "This round was a tie."
         } else if (playRound(playerChoice, computerChoice)) {     // Play round with player's selection
             playerScore += 1;
-            // playerScoreDisplay.textContent = `${playerScore}`;
-            displayRoundWinner.textContent = "You won this round!"
+            displayRoundWinner.textContent = "You won this round!";
         } else {
             computerScore += 1;
-            // computerScoreDisplay.textContent = `${computerScore}`;
-            displayRoundWinner.textContent = "You lost this round."
+            displayRoundWinner.textContent = "You lost this round.";
         };
+
+        // Display the current player and computer scores
         displayCurrentScore.textContent = `Player: ${playerScore}; Computer: ${computerScore}`
+
         // Check for 5 games won and end match
         if (playerScore === 5) {
             displayGameWinner.style.cssText = "font-weight: 900; font-size: 32px; text-align: center; color: green"
             displayGameWinner.textContent = "You win!";
-            buttons.forEach(button => button.disabled = true);
+            buttons.forEach(button => button.disabled = true); // Disable buttons to prevent play
         } 
         if (computerScore === 5) {
             displayGameWinner.style.cssText = "font-weight: 900; font-size: 32px; text-align: center; color: red"
